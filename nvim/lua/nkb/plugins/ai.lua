4a6af9ba8b3ccc5e77ed79ee8631bb7e8048300a
Nate Back <nback@insiderealestate.com>
2025-07-12 01:08:59 -0600

    Add a couple more plugins and split out some of the main ones to their own files

---
+++ nvim/lua/nkb/plugins/ai.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "olimorris/codecompanion.nvim",
+        opts = {},
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("codecompanion").setup({
+                strategies = {
+                    chat = {
+                        adapter = "anthropic",
+                    },
+                    inline = {
+                        adapter = "anthropic",
+                    },
+                }
+            })
+        end
+    },
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_suggestion = "<Tab>",
+                    clear_suggestion = "<C-]>",
+                    accept_word = "<C-j>",
+                },
+                ignore_filetypes = {},
+                color = {
+                    suggestion_color = "#ffffff",
+                    cterm = 244,
+                },
+                log_level = "info",
+                disable_inline_completion = false,
+                disable_keymaps = false,
+            })
+        end,
+    },
+
+}
